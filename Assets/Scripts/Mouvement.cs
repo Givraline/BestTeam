67ae8a46cad7ff84f18e7edcd2126d51e34988b2
Tsem12 <82233249+Tsem12@users.noreply.github.com>
2022-10-17 12:52:34 +0200

    Set up input system

---
+++ Assets/Scripts/Mouvement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mouvement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    [SerializeField] private InputActionAsset controler;
+
+    
+
+}
