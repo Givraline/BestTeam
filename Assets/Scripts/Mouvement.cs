6b36911edc96f015180852194f97822a57009be9
Tsem12 <tom.segaux60@gmail.com>
2022-10-17 22:18:52 +0200

    ADD Movement

--- Assets/Scripts/Mouvement.cs
+++ Assets/Scripts/Mouvement.cs
@@ -5,10 +5,37 @@ using UnityEngine.InputSystem;
 
 public class Mouvement : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
-    [SerializeField] private InputActionAsset controler;
+    [SerializeField] private PlayerInputActions controler;
+    [SerializeField] private InputAction move;
 
-    
+    private Vector2 mouvement;
+    private Rigidbody2D rb;
 
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        controler = new PlayerInputActions();
+    }
+    private void OnEnable()
+    {
+        move = controler.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
+
+    private void Update()
+    {
+        mouvement = move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(mouvement.x * speed, mouvement.y * speed);
+    }
 }
