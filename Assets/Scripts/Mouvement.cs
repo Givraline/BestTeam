3f0f9f8bff00fa86741aa5e450b1ef2eeb4ee3ef
Tsem12 <tom.segaux60@gmail.com>
2022-10-18 13:01:50 +0200

    ADD Lake

--- Assets/Scripts/Mouvement.cs
+++ Assets/Scripts/Mouvement.cs
@@ -8,6 +8,7 @@ public class Mouvement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private PlayerInputAction playerControler;
     [SerializeField] private InputAction move;
+    [SerializeField] private InputAction toggleNote;
 
     private Vector2 mouvement;
     private Rigidbody2D rb;
@@ -17,16 +18,21 @@ public class Mouvement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         playerControler = new PlayerInputAction();
+        playerControler.Player.ToggleNote.performed += toggle => Test();
     }
     private void OnEnable()
     {
         move = playerControler.Player.Move;
+        toggleNote = playerControler.Player.ToggleNote;
+        toggleNote.Enable();
         move.Enable();
+
     }
 
     private void OnDisable()
     {
         move.Disable();
+        toggleNote.Disable();
     }
 
     private void Update()
@@ -37,5 +43,10 @@ public class Mouvement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(mouvement.x * speed, mouvement.y * speed);
+    }
+    
+    private void Test()
+    {
+        Debug.Log("test");
     }
 }
