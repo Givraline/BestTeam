e5a536b8627d81b15e414a70d66009a40dd30d89
DanZo0x <dan.cheype@gmail.com>
2022-10-21 00:13:56 +0200

    Almost finished cam zoom

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] CinemachineVirtualCamera vcam;
+    [SerializeField] float maxFieldOfView;
+    [SerializeField] float minFieldOfView;
+    [SerializeField] float zoomSpeed;
+    [SerializeField] bool zoomTest;
+    private
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        cameraZoom();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "FishableArea")
+        {
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "FishableArea")
+        {
+            vcam.m_Lens.FieldOfView = 9;
+        }
+    }
+
+    private void cameraZoom()
+    {
+        if (zoomTest)
+        {
+            Debug.Log("cam");
+            //vcam.m_Lens.FieldOfView = Mathf.Clamp(vcam.m_Lens.FieldOfView - 3, 3, 9);
+            vcam.m_Lens.FieldOfView = 3;
+        }
+        else
+        {
+            //vcam.m_Lens.FieldOfView = Mathf.Lerp(vcam.m_Lens.FieldOfView + 6, 9, 3);
+            vcam.m_Lens.FieldOfView = 9;
+        }
     }
 }
