d816bcaaff43c0b2217ce46eb284188d07d23615
riufix <enguerrand.cousin@gmail.com>
2022-10-21 10:04:57 +0200

    update Player prefab + fish fished indicator

---
+++ Assets/Scripts/ObtainFish.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObtainFish : MonoBehaviour
+{
+    public GameObject[] fish;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
